e3867a453604043377daa8b87f0a9d2554762edf
noxiouswonder <85096215+noxiouswonder@users.noreply.github.com>
2021-07-01 19:35:27 +0530

    Delete backend directory

--- backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const order = require("../controllers/createOrder");
-
-const router = express.Router();
-
-router.post("/", order.create);
-router.get("/", order.get);
-
-module.exports = router;
