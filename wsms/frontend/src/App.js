512062ccb997ca9a75525ee1a6e5098d6249bb85
aryabhatta-dey <aryabhattadey35@gmail.com>
2021-07-03 13:11:59 +0530

    Fixed the removeItem bugs

--- wsms/frontend/src/App.js
+++ wsms/frontend/src/App.js
@@ -1,135 +1,155 @@
+import React from "react";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import "./index.css";
 
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-
-// Error messages
-const required = "This field is required";
-
-const App = () => {
+function App() {
   const {
     register,
+    control,
     handleSubmit,
-    watch,
     reset,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      item: [
+        {
+          itemName: "Name",
+          itemQuantity: "Quantity",
+          itemUnit: "Units",
+          itemValue: "Value",
+        },
+      ],
+    },
+  });
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  }; // your form submit function which will invoke after successful validation
+  // Default values absolutely needs to be supplied
+  // See tips section under React hook form v7 controller
+  // https://react-hook-form.com/api/usecontroller/controller
 
-  console.log(watch("example")); // you can watch individual input by pass the name of the input
+  const { fields, remove, append } = useFieldArray({
+    control,
+    name: "item",
+  });
 
-  const [indexes, setIndexes] = useState([]);
-  const [counter, setCounter] = useState(0);
+  const required = "This field is required";
 
-  const addItem = () => {
-    setIndexes((prevIndexes) => [...prevIndexes, counter]);
-    setCounter((prevCounter) => prevCounter + 1);
-  };
-
-  const removeItem = (index) => () => {
-    setIndexes((prevIndexes) => [
-      ...prevIndexes.filter((item) => item !== index),
-    ]);
-    setCounter((prevCounter) => prevCounter - 1);
-  };
-
-  const clearItems = () => {
-    setIndexes([]);
-  };
+  const onSubmit = (data) => alert(JSON.stringify(data));
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} onReset={reset}>
-      <div className="card m-3">
-        <div>
-          <h5 className="card-header">Order</h5>
-        </div>
-        <div className="card-body border-bottom">
-          <input
-            {...register("warehouseName", {
-              required: true,
-            })}
-            placeholder="Warehouse"
-          />
-          {errors?.warehouseName?.type === "required" && <p>{required}</p>}
-          <input
-            {...register("vendor", { required: true })}
-            placeholder="Vendor"
-          />
-          {errors?.vendor?.type === "required" && <p>{required}</p>}
-          <div>
-            <label>Dispatch date</label>
-            <input
-              {...register("dispatchDate", {
-                required: true,
-                pattern:
-                  "(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}",
-                //pattern is dd.mm.yyyy
-              })}
-              placeholder="dd.mm.yyyy"
-            />
-          </div>
-          <div>
-            <label>Delivery date</label>
-            <input
-              {...register("deliveryDate", {
-                required: true,
-                pattern:
-                  "(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}",
-                //pattern is dd.mm.yyyy
-              })}
-              placeholder="dd.mm.yyyy"
-            />
-          </div>
-        </div>
-        {indexes.map((index) => {
-          const fieldName = `item[${index}]`;
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Order </h1>
+
+      <input
+        {...register("warehouseName", {
+          required: true,
+        })}
+        placeholder="Warehouse"
+      />
+      {errors?.warehouseName?.type === "required" && <p>{required}</p>}
+
+      <input {...register("vendor", { required: true })} placeholder="Vendor" />
+      {errors?.vendor?.type === "required" && <p>{required}</p>}
+
+      <div>
+        <label>Dispatch date</label>
+        <input
+          {...register("dispatchDate", {
+            required: true,
+            pattern: "(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}",
+            //pattern is dd.mm.yyyy
+          })}
+          placeholder="dd.mm.yyyy"
+        />
+        <label>Delivery date</label>
+        <input
+          {...register("deliveryDate", {
+            required: true,
+            pattern: "(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}",
+            //pattern is dd.mm.yyyy
+          })}
+          placeholder="dd.mm.yyyy"
+        />
+      </div>
+
+      <ul>
+        {fields.map((object, index) => {
           return (
-            <fieldset name={fieldName} key={fieldName}>
-                <input
-                  {...register(`${fieldName}.itemName`)}
-                  placeholder="Item name"
-                />
-                <input
-                  type="number"
-                  {...register(`${fieldName}.itemQuantity`)}
-                  placeholder="Quantity"
-                />
-                <input
-                  type="number"
-                  {...register(`${fieldName}.itemUnit`)}
-                  placeholder="Unit"
-                />
-                <input
-                  type="number"
-                  {...register(`${fieldName}.itemValue`)}
-                  placeholder="Value"
-                />
-              <button type="button" onClick={removeItem(index)}>
-                Remove
+            <li key={object.id}>
+              <input
+                defaultValue={`${object.itemName}`} // make sure to set up defaultValue
+                {...register(`item[${index}].itemName`)}
+              />
+
+              <Controller
+                name={`item[${index}].itemQuantity`}
+                control={control}
+                defaultValue={object.itemQuantity} // make sure to set up defaultValue
+                render={({ field }) => <input {...field} />}
+              />
+
+              <Controller
+                name={`item[${index}].itemUnit`}
+                control={control}
+                defaultValue={object.itemUnit} // make sure to set up defaultValue
+                render={({ field }) => <input {...field} />}
+              />
+              <Controller
+                name={`item[${index}].itemValue`}
+                control={control}
+                defaultValue={object.itemValue} // make sure to set up defaultValue
+                render={({ field }) => <input {...field} />}
+              />
+
+              <button type="button" onClick={() => remove(index)}>
+                Remove item
               </button>
-            </fieldset>
+            </li>
           );
         })}
-        <button type="button" onClick={addItem}>
+      </ul>
+
+      <section>
+        <button
+          type="button"
+          onClick={() => {
+            append({
+              itemName: "Name",
+              itemQuantity: "Quantity",
+              itemUnit: "Units",
+              itemValue: "Value",
+            });
+          }}
+        >
           Add item
         </button>
-        <button type="button" onClick={clearItems}>
-          Clear all items
+
+        <button
+          type="button"
+          onClick={() =>
+            reset({
+              item: [
+                {
+                  itemName: "Name",
+                  itemQuantity: "Quantity",
+                  itemUnit: "Units",
+                  itemValue: "Value",
+                },
+              ],
+            })
+          }
+        >
+          Reset form
         </button>
-        <input
-          type="number"
-          {...register("totalValue", { required: true })}
-          placeholder="Total Value"
-        />
-        {errors?.totalValue?.type === "required" && <p>{required}</p>}
-        <input type="submit" value="Place order" />
-        <input type="reset" value="Reset form" />
-      </div>
+      </section>
+
+      <input
+        {...register("totalValue", { required: true })}
+        placeholder="Total Value"
+      />
+      {errors?.totalValue?.type === "required" && <p>{required}</p>}
+      <input type="submit" value="Place order" />
     </form>
   );
-};
+}
 
 export default App;
